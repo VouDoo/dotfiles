f2500b2f3f0b804b2b8448fcba9fdc4b13ea8e4b
VouDoo <maxgrymonprez@live.fr>
2023-08-18 20:21:12 +0200

    update nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -360,7 +360,7 @@ require("mason").setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { "clangd", "rust_analyzer", "pyright", "tsserver", "sumneko_lua", "gopls" }
+local servers = { "clangd", "rust_analyzer", "pyright", "tsserver", "lua_ls", "gopls" }
 
 -- Ensure the servers above are installed
 require("mason-lspconfig").setup({
@@ -388,22 +388,13 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require("lspconfig").sumneko_lua.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+require("lspconfig").lua_ls.setup({
   settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-        version = "LuaJIT",
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
+      Lua = {
+        diagnostics = {
         globals = { "vim" },
       },
       workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-      -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = { enable = false },
     },
   },
@@ -454,8 +445,6 @@ cmp.setup({
 
 -- nvim-tree setip
 require("nvim-tree").setup()
-
-vim.keymap.set("n", "<leader>e", require("nvim-tree").open, { desc = "[e] Open file explorer" })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
