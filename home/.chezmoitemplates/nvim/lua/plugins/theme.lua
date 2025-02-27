241457547c59324eaf550e2528abb15e21d273fa
Maxence Grymonprez <maxgrymonprez@live.fr>
2025-02-27 13:25:30 +0100

    Change Neovim theme to Rose Pine Moon

--- home/.chezmoitemplates/nvim/lua/plugins/theme.lua
+++ home/.chezmoitemplates/nvim/lua/plugins/theme.lua
@@ -1,12 +1,7 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine-moon")
+	end
 }
