d0bd8a24629f7950523eaacdc0f2eb3300e0179c
Maxence Grymonprez <maxgrymonprez@live.fr>
2025-07-05 15:02:05 +0200

    Edit fish abbreviations

--- home/private_dot_config/fish/abbreviations.fish
+++ home/private_dot_config/fish/abbreviations.fish
@@ -1,21 +1,13 @@
 # https://fishshell.com/docs/current/cmds/abbr.html
 
-# Core
-abbr -a cm chezmoi
+abbr -a cd z
 abbr -a find fd
 abbr -a grep rg
+abbr -a tree ls --tree
 abbr -a vi nvim
 abbr -a vim nvim
-abbr -a tree ls --tree
-
-# Moving around...
-abbr -a .. 'cd ..'
-abbr -a ... 'cd ../../'
-abbr -a .... 'cd ../../../'
-abbr -a ..... 'cd ../../../../'
-abbr -a - 'cd -'
-
-# Extra
+abbr -a lazygit lgit
+abbr -a cm chezmoi
 if type -q bluetoothctl
     abbr -a bluetooth bluetoothctl
 end
