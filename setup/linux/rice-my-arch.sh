f82dad1c0c36a50a971916c5bdade74a566a43fb
Maxence Grymonprez <maxgrymonprez@live.fr>
2026-07-14 15:40:10 +0200

    Update linux setup script for Arch Linux

--- setup/linux/rice-my-arch.sh
+++ setup/linux/rice-my-arch.sh
@@ -15,7 +15,6 @@
 #
 ###############################################################################
 
-# Exit immediately if any command fails
 set -e
 
 # System Synchronization
@@ -41,20 +40,28 @@ paru -S --needed ly
 sudo systemctl enable ly@tty2.service
 
 # Desktop Environment (Wayland Window Manager & Portal)
-# Niri is a scrollable-tiling compositor. XDG portals handle screensharing and file dialogues.
-paru -S --needed niri xwayland-satellite xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+# Niri is a scrollable-tiling compositor.
+# XDG portals handle screensharing and file dialogues.
+# kanshi allows you to define output profiles that are automatically enabled and disabled on hotplug.
+paru -S --needed niri xwayland-satellite xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk kanshi
 
 # Core Shell & Aesthetics
 # Installs 0xProto Nerd Font for UI iconography, and Noctalia as the top-bar/shell.
 paru -S --needed ttf-0xproto-nerd noctalia-git
 
 # Clipboard Management
-# wl-clipboard provides copy/paste backends; cliphist acts as the local clipboard history daemon.
+# wl-clipboard provides copy/paste backends.
+# cliphist acts as the local clipboard history daemon.
 paru -S --needed wl-clipboard cliphist
 
+# Interactive Shell
+paru -S --needed fish
+echo "Set Fish as default shell for your user:"
+chsh --shell /usr/bin/fish
+
 # Modern CLI Tooling
-# Modern terminal utilities replacing standard coreutils (ls -> eza, cat -> bat, cd -> zoxide, etc.)
-paru -S --needed fish starship bat btop eza fd fzf ripgrep zoxide rsync git-delta tlrc-bin fastfetch
+# Some utilities replace standard coreutils (ls -> eza, cat -> bat, cd -> zoxide, etc.).
+paru -S --needed starship bat btop eza fd fzf ripgrep zoxide rsync git-delta tlrc-bin fastfetch
 
 # Text Editors
 # Neovim and Helix for modal terminal-based editing.
@@ -66,9 +73,6 @@ paru -S --needed impala bluetui pavucontrol yazi lazygit
 
 # Core Productivity Apps
 # Ghostty (Terminal), Zen (Browser), KeePassXC (Credentials), and Rclone (Cloud Storage Sync).
-paru -S --needed ghostty zen-browser-bin keepassxc qt5-wayland rclone
-
-echo "Set Fish as default shell for your user:"
-chsh --shell /usr/bin/fish
+paru -S --needed ghostty zen-browser-bin keepassxc qt5-wayland rclone localsend-bin
 
 echo "Base installation complete! Please reboot your system."
