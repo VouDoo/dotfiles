6005f1ea3350dd688d0bd1f52809bba93eded79f
Maxence Grymonprez <maxgrymonprez@live.fr>
2026-09-09 16:21:18 +0200

    Add icon theme package to Linux setup script

--- setup/linux/rice-my-arch.sh
+++ setup/linux/rice-my-arch.sh
@@ -55,6 +55,8 @@ paru -S --needed ttf-0xproto-nerd noctalia
 # Installs GTK theme
 # Noctalia can automatically theme them using the adw-gtk3 theme.
 paru -S --needed adw-gtk-theme nwg-look
+# Install icon theme
+paru -S --needed papirus-icon-theme
 
 # Clipboard Management
 # wl-clipboard provides copy/paste backends.
